7c48b464fc648f1558334bf30a1ea32c6f521e83
Roxanne <tran.rox@gmail.com>
2021-04-26 08:14:15 -0400

    .

--- components/home/Welcome.js
+++ components/home/Welcome.js
@@ -36,66 +36,59 @@ export const WelcomeWrapper = styled.div`
   position: relative;
   margin: 420px auto 320px;
   display: flex;
-  max-width: 1100px;
+  max-width: 1200px;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
-  padding: 0 5%;
-
-  @media screen and (max-width: 768px) {
-    margin-top: 12rem;
-    justify-content: flex-start;
-    flex-direction: column;
-  }
-
+  padding: 0 1%;
   .image {
     box-shadow: -35px 35px 0 0 rgba(122, 207, 255, 0.3);
-
-    @media screen and (max-width: 768px) {
-      box-shadow: 0 0 0 0 rgba(122, 207, 255, 0.3);
-    }
-
     &:hover {
       box-shadow: 0 0 0 0 rgba(122, 207, 255, 0.3);
     }
   }
-
   .text {
     flex: 1;
     margin-left: 4rem;
     display: flex;
     flex-direction: column;
-
-    @media screen and (max-width: 1024px) {
-      margin-left: 3rem;
-    }
-
-    @media screen and (max-width: 768px) {
-      margin-left: 0;
-      padding: 0 10%;
-    }
-
     h2 {
       color: var(--blue);
       margin-bottom: 1rem;
-
-      @media screen and (max-width: 1024px) {
-        font-size: 1.75rem;
-      }
-
-      @media screen and (max-width: 768px) {
-        margin-top: 4rem;
-      }
     }
-
     p.quote {
       font-weight: bold;
       font-style: italic;
       margin-bottom: 1rem;
     }
-
     span {
       font-weight: bold;
+    }
+  }
+  @media screen and (max-width: 1024px) {
+    padding: 0 5%;
+    margin-bottom: 15rem;
+    .image {
+      box-shadow: 0 0 0 0 rgba(122, 207, 255, 0.3);
+    }
+    .text {
+      margin-left: 3rem;
+    }
+    h2 {
+      font-size: 1.75rem;
+    }
+  }
+  @media screen and (max-width: 768px) {
+    margin-top: 12rem;
+    justify-content: flex-start;
+    flex-direction: column;
+
+    .text {
+      margin-left: 0;
+      padding: 0 10%;
+    }
+    h2 {
+      margin-top: 4rem;
     }
   }
 `
