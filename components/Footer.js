b34f1b8866f4e1196e0eb35788c4bc3f8e02a018
Roxanne <tran.rox@gmail.com>
2021-04-28 14:35:39 -0400

    .

--- components/Footer.js
+++ components/Footer.js
@@ -132,14 +132,14 @@ export default function Footer() {
             <Image src='/img/giving-icon.webp' width={35} height={35} />
           </div>
         </a>
-        <a href={paths.facebookPage} target='_blank'>
-          <div className='icon'>
-            <Image src='/img/facebook-icon.webp' width={30} height={30} />
-          </div>
-        </a>
         <a href={paths.instagramAccount} target='_blank'>
           <div className='icon'>
             <Image src='/img/instagram-icon.png' width={35} height={35} />
+          </div>
+        </a>
+        <a href={paths.facebookPage} target='_blank'>
+          <div className='icon'>
+            <Image src='/img/facebook-icon.webp' width={30} height={30} />
           </div>
         </a>
         <a href={paths.twitterAccount} target='_blank'>
