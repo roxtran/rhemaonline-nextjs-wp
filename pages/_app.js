c1fc40fcc3c35597b902b24c26b6836060a12dbe
Roxanne <tran.rox@gmail.com>
2021-05-07 03:22:16 -0400

    added sidebar to single note page

--- pages/_app.js
+++ pages/_app.js
@@ -14,8 +14,8 @@ function MyApp({ Component, pageProps }) {
   )
 }
 
-export function reportWebVitals(metric) {
-  console.log(metric)
-}
+// export function reportWebVitals(metric) {
+//   console.log(metric)
+// }
 
 export default MyApp
