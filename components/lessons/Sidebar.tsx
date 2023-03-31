1df9f3ab98a43ddc6491e26648a880ec0febd307
Roxanne Tran <tran.rox@gmail.com>
2023-03-31 13:01:41 -0400

    Update Sidebar width

--- components/lessons/Sidebar.tsx
+++ components/lessons/Sidebar.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import styled from 'styled-components'
 import NoteType from 'types/note'
+import { rem } from 'styles/GlobalStyle'
+
 
 interface Props {
   title: string
@@ -38,8 +40,8 @@ const SidebarWrapper = styled.div`
   position: sticky;
   top: 5rem;
   margin-top: 6.65rem;
-  width: 300px;
-  border-top: 4px dashed var(--mint);
+  width: ${rem(325)};
+  border-top: ${rem(4)} dashed var(--mint);
   padding: 1rem 1rem 0;
   h3 {
     margin-bottom: 0.5rem;
@@ -48,9 +50,9 @@ const SidebarWrapper = styled.div`
     list-style: none;
   }
   li {
-    padding: 16px 0;
+    padding: ${rem(16)} 0;
     &:not(:last-child) {
-      border-bottom: 1px solid var(--mint);
+      border-bottom: ${rem(1)} solid var(--mint);
     }
   }
   p {
@@ -60,11 +62,11 @@ const SidebarWrapper = styled.div`
     font-style: italic;
     margin-top: 0.25rem;
   }
-  @media screen and (max-width: 1024px) {
-    width: 300px;
+  @media screen and (max-width: ${rem(1024)}) {
+    width: ${rem(325)};
     padding: 2rem 3%;
   }
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: ${rem(768)}) {
     padding: 2rem 0;
     margin-top: 2rem;
   }
