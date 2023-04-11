d2a050a2a82a359d9fa1aecc503addc935617b5d
Roxanne Tran <tran.rox@gmail.com>
2023-04-11 15:32:05 -0400

    Update slide 3 image

--- data/slides.ts
+++ data/slides.ts
@@ -1,28 +1,28 @@
 export const slides = [
   {
-    imgUrl: "/img/slides/1.png",
-    h1: "Welcome to Rhema Canada",
+    imgUrl: '/img/slides/1.png',
+    h1: 'Welcome to Rhema Canada',
     p:
-      "Rhema Canada is a world class people-centered organization with people at the heart of what we do. We change lives in the community and the world with a focus on economic empowerment, ensuring children in areas of poverty receive quality education and supporting the embodiment of spiritual excellence.",
+      'Rhema Canada is a world class people-centered organization with people at the heart of what we do. We change lives in the community and the world with a focus on economic empowerment, ensuring children in areas of poverty receive quality education and supporting the embodiment of spiritual excellence.',
   },
   {
-    imgUrl: "/img/slides/2.png",
-    h1: "Where GOD Matters",
-    p: "Welcome to Rhema Canada, where God matters.",
+    imgUrl: '/img/slides/2.png',
+    h1: 'Where GOD Matters',
+    p: 'Welcome to Rhema Canada, where God matters.',
   },
   {
-    imgUrl: "/img/slides/3.png",
-    h1: "Where PEOPLE Matter",
-    p: "Rhema, where loving, valuing and serving is what we do.",
+    imgUrl: '/img/slides/3.jpg',
+    h1: 'Where PEOPLE Matter',
+    p: 'Rhema, where loving, valuing and serving is what we do.',
   },
   {
-    imgUrl: "/img/slides/4.png",
-    h1: "Where LIFE Matters",
-    p: "Rhema, where living life to the fullest is how we live.",
+    imgUrl: '/img/slides/4.png',
+    h1: 'Where LIFE Matters',
+    p: 'Rhema, where living life to the fullest is how we live.',
   },
   {
-    imgUrl: "/img/slides/5.png",
-    h1: "Where LIFE Matters",
-    p: "Rhema, where living life to the fullest is how we live.",
+    imgUrl: '/img/slides/5.png',
+    h1: 'Where LIFE Matters',
+    p: 'Rhema, where living life to the fullest is how we live.',
   },
-];
+]
