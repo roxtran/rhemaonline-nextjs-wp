27d5943e59dc0c400e58e9b8123a8877b174a36f
Roxanne Tran <tran.rox@gmail.com>
2023-04-14 11:52:31 -0400

    Update videoId fallback to blocking

--- pages/sermons/videos/[videoId].tsx
+++ pages/sermons/videos/[videoId].tsx
@@ -1,78 +1,90 @@
-import Meta from 'components/common/meta'
-import styled from 'styled-components'
-import { Container, rem } from 'styles/GlobalStyle'
-import Link from 'next/link'
-import Image from 'next/image'
-import popularVideos from 'data/popular-videos'
-import recentVideos from 'data/recent-videos'
-import VideoType from 'types/video'
-import { IoArrowBackOutline } from 'react-icons/io5'
-import paths from 'paths'
-import { formatDate } from 'utils/formatter'
+import Meta from "components/common/meta";
+import styled from "styled-components";
+import { Container, rem } from "styles/GlobalStyle";
+import Link from "next/link";
+import Image from "next/image";
+import popularVideos from "data/popular-videos";
+import recentVideos from "data/recent-videos";
+import VideoType from "types/video";
+import { IoArrowBackOutline } from "react-icons/io5";
+import paths from "paths";
+import { formatDate } from "utils/formatter";
 
 interface Props {
-  video: VideoType
+  video: VideoType;
 }
 
 type Params = {
   params: {
-    videoId: string
-  }
-}
+    videoId: string;
+  };
+};
 
 export default function Sermons({ video }: Props) {
-  return <>
-    <Meta title={video[0].snippet.title + ' - Rhema - Changing & Affecting Lives!'} />
-    <VideoContainer>
-      <div className='wrapper'>
-        <Link href={paths.sermons} className='btn-back'>
-
-          <IoArrowBackOutline className='icon-back' />Go back
-        </Link>
-        <VideoWrapper>
-          <iframe
-            width='100%'
-            height='100%'
-            src={`https://www.youtube.com/embed/${video[0].id.videoId}?rel=0&showinfo=1&autoplay=1&loop=0`}
-            title='iframe video player'
-            frameBorder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          ></iframe>
-        </VideoWrapper>
-        <div className='text-wrapper'>
-          <h3 className='title' dangerouslySetInnerHTML={{ __html: video[0].snippet.title }} />
-          <div className='subtitle'>Rhema Christian Ministries • {formatDate(video[0].snippet.publishedAt)}</div>
-          <div className='desc'>
-            Thank you for supporting Rhema Christian Ministries.
-            <br /> If you're looking for ways to give, simply click here:{' '}
-            <Link href='https://rhemaonline.ca/giving/'>
-              https://rhemaonline.ca/giving/
+  return (
+    <>
+      <Meta title={video[0].snippet.title + " - Rhema - Changing & Affecting Lives!"} />
+      <VideoContainer>
+        <div className="wrapper">
+          <Link href={paths.sermons} className="btn-back">
+            <IoArrowBackOutline className="icon-back" />
+            Go back
+          </Link>
+          <VideoWrapper>
+            <iframe
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed/${video[0].id.videoId}?rel=0&showinfo=1&autoplay=1&loop=0`}
+              title="iframe video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            ></iframe>
+          </VideoWrapper>
+          <div className="text-wrapper">
+            <h3 className="title" dangerouslySetInnerHTML={{ __html: video[0].snippet.title }} />
+            <div className="subtitle">
+              Rhema Christian Ministries • {formatDate(video[0].snippet.publishedAt)}
+            </div>
+            <div className="desc">
+              Thank you for supporting Rhema Christian Ministries.
+              <br /> If you're looking for ways to give, simply click here:{" "}
+              <Link href="https://rhemaonline.ca/giving/">https://rhemaonline.ca/giving/</Link>
+            </div>
+          </div>
+          <div className="button-wrapper">
+            <Link
+              href="https://tithe.ly/give_new/www/#/tithely/give-one-time/645887?kiosk=1"
+              target="__blank"
+            >
+              <div className="icon give-icon">
+                <Image
+                  src="/img/icons/give.png"
+                  width={48}
+                  height={48}
+                  alt="give"
+                  objectFit="cover"
+                />
+              </div>
+            </Link>
+            <Link
+              href="https://visitor.r20.constantcontact.com/manage/optin?v=001BRl1GnvqHBSmpUgt0bWkn1MfFFgc1eWZnLBSjeq8hPkIyEhXoERcP_p-E94Tuo8i-n6WMjixEVWfoTusoH241Xry9A65AaaC8uzGvinLhOc%3D"
+              target="__blank"
+            >
+              <div className="icon subscribe-icon">
+                <Image
+                  src="/img/icons/subscribe.png"
+                  width={48}
+                  height={48}
+                  alt="subscribe"
+                  objectFit="cover"
+                />
+              </div>
             </Link>
           </div>
         </div>
-        <div className='button-wrapper'>
-          <Link
-            href='https://tithe.ly/give_new/www/#/tithely/give-one-time/645887?kiosk=1'
-            target='__blank'>
-
-            <div className='icon give-icon'>
-              <Image src='/img/icons/give.png' width={48} height={48} alt='give' objectFit='cover' />
-            </div>
-
-          </Link>
-          <Link
-            href='https://visitor.r20.constantcontact.com/manage/optin?v=001BRl1GnvqHBSmpUgt0bWkn1MfFFgc1eWZnLBSjeq8hPkIyEhXoERcP_p-E94Tuo8i-n6WMjixEVWfoTusoH241Xry9A65AaaC8uzGvinLhOc%3D'
-            target='__blank'>
-
-            <div className='icon subscribe-icon'>
-              <Image src='/img/icons/subscribe.png' width={48} height={48} alt='subscribe' objectFit='cover' />
-            </div>
-
-          </Link>
-        </div>
-      </div>
-    </VideoContainer>
-  </>;
+      </VideoContainer>
+    </>
+  );
 }
 
 const VideoContainer = styled(Container)`
@@ -140,7 +152,7 @@ const VideoContainer = styled(Container)`
   .video-wrapper:hover {
     transform: scale(1.025);
   }
-`
+`;
 
 const VideoWrapper = styled.div`
   width: 100%;
@@ -153,26 +165,26 @@ const VideoWrapper = styled.div`
     width: 90vw;
     height: calc(90vw * 9 / 16);
   }
-`
+`;
 
-const videos = popularVideos
-videos.push(...recentVideos)
+const videos = popularVideos;
+videos.push(...recentVideos);
 
 export const getStaticProps = async ({ params }: Params) => {
-  const { videoId } = params
-  const video = videos.filter((video) => video.id.videoId === videoId)
+  const { videoId } = params;
+  const video = videos.filter((video) => video.id.videoId === videoId);
 
   return {
     props: {
       video: video
     }
-  }
-}
+  };
+};
 
 export const getStaticPaths = async () => {
   const paths = videos.map((video) => ({
     params: { videoId: video.id.videoId }
-  }))
+  }));
   // console.log('!! paths', paths)
-  return { paths, fallback: false }
-}
+  return { paths, fallback: "blocking" };
+};
