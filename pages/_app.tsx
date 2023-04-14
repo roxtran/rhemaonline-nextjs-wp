ab6a8d0bddab1e8a021f7e41542e9cf77679c95c
Roxanne Tran <tran.rox@gmail.com>
2023-04-14 17:43:41 -0400

    Fix AppProps issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,44 @@
-import Head from 'next/head'
-import 'styles/_variables.css'
-import 'styles/GlobalStyle'
-import GlobalStyle from 'styles/GlobalStyle'
-import Layout from 'components/common/Layout'
-import { AppProps } from 'next/dist/next-server/lib/router/router'
-import { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
+import Head from "next/head";
+import "styles/_variables.css";
+import "styles/GlobalStyle";
+import GlobalStyle from "styles/GlobalStyle";
+import Layout from "components/common/Layout";
+import { AppProps } from "next/dist/shared/lib/router/router";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 // import SurveyModal from 'components/common/Modals/HomeModal'
 // chatbot
-import Chatbot from 'components/Chatbot'
+import Chatbot from "components/Chatbot";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  const [openModal, setOpenModal] = useState(false)
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     const showModal = setTimeout(() => {
-      setOpenModal(true)
-    }, 30000)
+      setOpenModal(true);
+    }, 30000);
 
     return () => {
-      clearTimeout(showModal)
-    }
-  }, [])
+      clearTimeout(showModal);
+    };
+  }, []);
 
   return (
     <>
       <Head>
         {/* Responsive meta tag */}
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* bootstrap CDN */}
         <link
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css'
-          rel='stylesheet'
-          integrity='sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1'
-          crossOrigin='anonymous'
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossOrigin="anonymous"
         />
         <script
-          src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'
-          integrity='sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW'
-          crossOrigin='anonymous'
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+          crossOrigin="anonymous"
         ></script>
       </Head>
       <Layout>
@@ -55,11 +55,11 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         {/* <SurveyModal openModal={openModal} setOpenModal={setOpenModal} /> */}
       </Layout>
     </>
-  )
+  );
 }
 
 // export function reportWebVitals(metric) {
 //   console.log(metric)
 // }
 
-export default MyApp
+export default MyApp;
