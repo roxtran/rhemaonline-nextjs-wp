d6b4a941a90d8aaef299e062926f61c997604a80
Roxanne Tran <tran.rox@gmail.com>
2023-10-04 22:03:50 -0400

    Add CC image domain

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: [process.env.WP_DOMAIN, process.env.CC_DOMAIN, process.env.YT_IMG]
+    domains: [process.env.WP_DOMAIN, process.env.CC_DOMAIN, process.env.YT_IMG, process.env.CC_IMG]
   }
-}
+};
