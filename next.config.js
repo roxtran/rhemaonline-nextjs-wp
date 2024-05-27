46d521fde629ca8f7b78032e566966e28e327903
Roxanne Tran <tran.rox@gmail.com>
2024-05-27 17:59:14 -0400

    Update meta

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   images: {
-    domains: [process.env.WP_DOMAIN, process.env.CC_DOMAIN, process.env.YT_IMG, process.env.CC_IMG]
-  }
+    domains: [process.env.WP_DOMAIN, process.env.CC_DOMAIN, process.env.YT_IMG, process.env.CC_IMG],
+  },
+  env: {
+    HOME_URL: process.env.HOME_URL,
+  },
 };
