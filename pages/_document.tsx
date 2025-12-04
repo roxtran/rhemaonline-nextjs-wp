34378a076666a550b106d7d3a823c1cedb40bfd8
Roxanne Tran <tran.rox@gmail.com>
2025-12-04 18:32:35 -0500

    Add Google Tag Manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -47,6 +47,22 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap"
             rel="stylesheet"
           />
+          {/* <!-- Google Tag Manager --> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w, d, s, l, i) {
+                      w[l] = w[l] || [];
+                      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+                      var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s),
+                        dl = l != "dataLayer" ? "&l=" + l : "";
+                      j.async = true;
+                      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+                      f.parentNode.insertBefore(j, f);
+                    })(window, document, "script", "dataLayer", "GTM-M8C3QGZ6");`,
+            }}
+          ></script>
+          {/* <!-- End Google Tag Manager --> */}
           <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
@@ -65,6 +81,16 @@ export default class MyDocument extends Document {
           ></script>
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M8C3QGZ6"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
